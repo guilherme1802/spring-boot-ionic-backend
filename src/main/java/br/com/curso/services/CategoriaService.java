68105de9a3008b8235225cfee8265fac3eb97259
Guilherme Rocha <gui1802@gmail.com>
2019-07-25 14:58:11 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/br/com/curso/services/CategoriaService.java
+++ src/main/java/br/com/curso/services/CategoriaService.java
@@ -32,10 +32,16 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+	/*
 	public Categoria update(Categoria obj){
 		find(obj.getId());
 		return repo.save(obj);
+	}*/
+	
+	public Categoria update(Categoria obj){
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id){
@@ -60,5 +66,8 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto){
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-
+	
+	private void updateData(Categoria newObj, Categoria obj){
+		newObj.setNome(obj.getNome());
+	}
 }
